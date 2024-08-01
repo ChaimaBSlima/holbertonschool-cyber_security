bbd8558d478343e983247ecf6984f5806ec7c170
ChaimaBS <chaimabslima@outlook.fr>
2024-08-01 13:51:12 +0100

    task3: 3. We need an SSH key pair! Only 2 line fix

--- cybersecurity_basics/0x00_introduction_cybersecurity/3-gen_key.sh
+++ cybersecurity_basics/0x00_introduction_cybersecurity/3-gen_key.sh
@@ -1,19 +1,2 @@
 #!/bin/bash
-
-if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 <key_name>"
-    exit 1
-fi
-
-KEY_NAME=$1
-
-ssh-keygen -t rsa -b 4096 -f "$KEY_NAME" -N ""
-
-if [ $? -ne 0 ]; then
-    echo "Failed to generate SSH key pair"
-    exit 1
-fi
-
-echo "SSH key pair generated successfully:"
-echo "Private key: $KEY_NAME"
-echo "Public key: $KEY_NAME.pub"
+ssh-keygen -t rsa -b 4096 -f "$1" -N "" && echo "SSH key pair generated successfully: Private key: $1, Public key: $1.pub" || echo "Failed to generate SSH key pair"
