a6d77d81d77400c531279f7225167158c9a4616c
ChaimaBS <chaimabslima@outlook.fr>
2024-08-02 09:03:25 +0100

    task 3 fix without printf

--- cybersecurity_basics/0x00_introduction_cybersecurity/3-gen_key.sh
+++ cybersecurity_basics/0x00_introduction_cybersecurity/3-gen_key.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ssh-keygen -t rsa -b 4096 -f "$1" -N "" && printf "SSH key pair generated successfully: Private key: %s, Public key: %s.pub\n" "$1" "$1" || printf "Failed to generate SSH key pair\n"
+ssh-keygen -t rsa -b 4096 -f "$1" -N ""
