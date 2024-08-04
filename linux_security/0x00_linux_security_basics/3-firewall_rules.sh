0396eff3af28ae42fcecfa6f3b087d60866c4af6
ChaimaBS <chaimabslima@outlook.fr>
2024-08-04 17:25:38 +0100

    task 3: 2 line desired

--- linux_security/0x00_linux_security_basics/3-firewall_rules.sh
+++ linux_security/0x00_linux_security_basics/3-firewall_rules.sh
@@ -1,8 +1,2 @@
 #!/bin/bash
-
-if [ "$(id -u)" -ne 0 ]; then
-  echo "This script must be run as root"
-  exit 1
-fi
-iptables -t security -L -v
-
+[ "$(id -u)" -ne 0 ] && { echo "This script must be run as root"; exit 1; }; iptables -t security -L -v
