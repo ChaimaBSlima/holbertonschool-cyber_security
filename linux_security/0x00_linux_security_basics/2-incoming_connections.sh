7258430d9686179a7b79dca9514160bb8bce4b54
ChaimaBS <chaimabslima@outlook.fr>
2024-08-04 17:22:36 +0100

    task 2: 2 line desired

--- linux_security/0x00_linux_security_basics/2-incoming_connections.sh
+++ linux_security/0x00_linux_security_basics/2-incoming_connections.sh
@@ -1,16 +1,2 @@
 #!/bin/bash
-
-# Check if the script is run as root
-if [ "$(id -u)" -ne 0 ]; then
-  echo "This script must be run as root"
-  exit 1
-fi
-
-# Allow incoming TCP connections on port 80
-iptables -A INPUT -p tcp --dport 80 -j ACCEPT
-
-# Drop all other incoming connections
-iptables -P INPUT DROP
-
-echo "Rules updated"
-
+[ "$(id -u)" -ne 0 ] && echo "This script must be run as root" && exit 1; iptables -A INPUT -p tcp --dport 80 -j ACCEPT && iptables -P INPUT DROP && echo "Rules updated"
