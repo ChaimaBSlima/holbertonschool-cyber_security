aa1bd23b9cc5d6fd78633a846335555a1f5ee46d
ChaimaBS <chaimabslima@outlook.fr>
2024-08-04 17:24:50 +0100

    task 2: ufw used

--- linux_security/0x00_linux_security_basics/2-incoming_connections.sh
+++ linux_security/0x00_linux_security_basics/2-incoming_connections.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-[ "$(id -u)" -ne 0 ] && echo "This script must be run as root" && exit 1; sudo iptables -A INPUT -p tcp --dport 80 -j ACCEPT && sudo iptables -P INPUT DROP && echo "Rules updated"
+[ "$(id -u)" -ne 0 ] && echo "This script must be run as root" && exit 1; sudo ufw allow 80/tcp && sudo ufw default deny incoming && echo "Rules updated"
