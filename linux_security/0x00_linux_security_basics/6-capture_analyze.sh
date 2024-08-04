ba9be35b369624bba53dfb37cde2f1aa12a86363
ChaimaBS <chaimabslima@outlook.fr>
2024-08-04 17:29:32 +0100

    task 6: 2 line desired

--- linux_security/0x00_linux_security_basics/6-capture_analyze.sh
+++ linux_security/0x00_linux_security_basics/6-capture_analyze.sh
@@ -1,11 +1,2 @@
 #!/bin/bash
-
-# Check if the script is run as root
-if [ "$EUID" -ne 0 ]; then
-  echo "Please run as root"
-  exit
-fi
-
-# Capture and analyze network traffic
-tcpdump -c 5 
-
+[ "$EUID" -ne 0 ] && { echo "Please run as root"; exit 1; }; sudo tcpdump -c 5
