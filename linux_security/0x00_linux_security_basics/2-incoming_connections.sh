dec6ae9d4e1d3ae0cca2e2ccae21b8d72056d3a0
ChaimaBS <chaimabslima@outlook.fr>
2024-08-04 17:23:53 +0100

    task 2: sudo used

--- linux_security/0x00_linux_security_basics/2-incoming_connections.sh
+++ linux_security/0x00_linux_security_basics/2-incoming_connections.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-[ "$(id -u)" -ne 0 ] && echo "This script must be run as root" && exit 1; iptables -A INPUT -p tcp --dport 80 -j ACCEPT && iptables -P INPUT DROP && echo "Rules updated"
+[ "$(id -u)" -ne 0 ] && echo "This script must be run as root" && exit 1; sudo iptables -A INPUT -p tcp --dport 80 -j ACCEPT && sudo iptables -P INPUT DROP && echo "Rules updated"
