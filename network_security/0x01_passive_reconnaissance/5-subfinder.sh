918a1625d766675d36e4ab6162b16d84f986c8f2
ChaimaBS <chaimabslima@outlook.fr>
2024-08-18 17:19:24 +0100

    task 5

--- network_security/0x01_passive_reconnaissance/5-subfinder.sh
+++ network_security/0x01_passive_reconnaissance/5-subfinder.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-subfinder -d "$1" | while read -r subdomain; do dig +short "$subdomain" | awk -v host="$subdomain" 'NR==1{print host "," $0}'; done > "$1.txt"
+subfinder -d $1 | while read -r subdomain; do dig +short "$subdomain" | awk -v host="$subdomain" 'NR==1{print host "," $0}'; done > "$1.txt"
