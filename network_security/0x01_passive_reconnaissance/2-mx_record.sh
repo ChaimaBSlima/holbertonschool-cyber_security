635e37499de8a09b5927e6ce515ec90eff7c46a5
ChaimaBS <chaimabslima@outlook.fr>
2024-08-18 17:11:47 +0100

    task 2

--- network_security/0x01_passive_reconnaissance/2-mx_record.sh
+++ network_security/0x01_passive_reconnaissance/2-mx_record.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-nslookup -query=mx "$1" | awk '/^Server:/ {sub(/^Server: /, "Server: "); $1=""; print $0} /^Address: / {sub(/^Address: /, "Address: "); $1=""; print $0} /mail exchanger/ {sub(/^.*mail exchanger = /, "mail exchanger = "); $1=""; print $0}'
+nslookup -query=mx "$1" | awk '/^Server:/ {sub(/^Server: /, "Server: "); print $0} /^Address: / {sub(/^Address: /, "Address: "); print $0} /mail exchanger/ {sub(/^.*mail exchanger = /, "mail exchanger = "); print}'
