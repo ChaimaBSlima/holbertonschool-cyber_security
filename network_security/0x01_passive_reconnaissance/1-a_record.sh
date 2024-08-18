6bda24ed4c6b5554e67caa3eb5dc8f84c8a1742a
ChaimaBS <chaimabslima@outlook.fr>
2024-08-18 17:00:00 +0100

    task 1

--- network_security/0x01_passive_reconnaissance/1-a_record.sh
+++ network_security/0x01_passive_reconnaissance/1-a_record.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-nslookup "$1" | awk '/^Server:/ {print $0} /^Address: / {print "Address: " $2}' | sed -e 's/^Address: /Address: /' -e 's/Name:   /Name:   '"$1"'\nAddress: /' -e 's/^Server:/Server: 8.8.8.8\nAddress: 8.8.8.8#53\n\nNon-authoritative answer:\nName:   '"$1"'\n/'
+{ echo "Server: $(nslookup "$1" | awk '/^Server:/ {print $2}')" ; echo "Address: $(nslookup "$1" | awk '/^Address: / {print $2}' | tr '\n' ' ')"; }
